68126215f2ea036ee7a8bc735be25328344247eb
Paul Hidalgo <paul_hidalgo@trendmicro.com>
2017-03-28 23:55:19 +0800

    Cleaning Up Part 1

--- cloudtrail.js
+++ cloudtrail.js
@@ -2,25 +2,24 @@
 var AWS = require( 'aws-sdk' );
 exports.disable = ( event, context, callback ) => {
 	console.log( 'Cloudwatch Disable Event' );
-	//	console.log( event );
-	//	console.log( event.id );
+	// Get Variables
 	var BlockARN = process.env.BlockARN;
 	var iam = new AWS.IAM( );
 	var cloudtrail = new AWS.CloudTrail( { apiVersion: '2013-11-01' } );
 	var eventName = event.detail.eventName;
 	var FunctionName = process.env.FunctionName;
 	var lambda = new AWS.Lambda( { apiVersion: '2015-03-31' } );
+
+
 	if ( eventName == 'StopLogging' ) {
 		var TrailARN = event.detail.requestParameters.name;
-		console.log( 'ARN: ' + TrailARN );
+		console.log( 'ARN: ' + TrailARN + 'User: ' + user );
 		var user = event.detail.userIdentity.userName;
-		console.log( 'User: ' + user );
-		console.log( 'Start Logging of: ' + TrailARN );
+		var logmessage = 'Cloudtrail ' + TrailARN + ' was disabled by user ' + user + ', Access was revoked';
 		var params = {
 			Name: TrailARN /* required */
 		};
-		var logmessage = 'Cloudtrail ' + TrailARN + ' was disabled by user ' + user + ', Access was revoked';
-		console.log( 'Log Message' );
+
 		cloudtrail.startLogging( params, function( err, data ) {
 			if ( err ) { console.log( err, err.stack ); } // an error occurred
 			else {
@@ -28,6 +27,8 @@ exports.disable = ( event, context, callback ) => {
 					PolicyArn: BlockARN,
 					UserName: user
 				};
+
+
 				iam.attachUserPolicy( params, function( err, data ) {
 					if ( err ) { console.log( err, err.stack ); } // an error occurred
 					else {
@@ -35,6 +36,8 @@ exports.disable = ( event, context, callback ) => {
 							FunctionName: FunctionName,
 							Payload: '{"message": "' + logmessage + '", "subject": "Cloudtrail Alert for ' + TrailARN + '"}'
 						};
+
+
 						lambda.invoke( messageParams, function( err, data ) {
 							if ( err ) console.log( err, err.stack ); // an error occurred
 							else console.log( data ); // successful response
--- restrictedssh.js
+++ restrictedssh.js
@@ -1,6 +1,7 @@
 'use strict';
 var AWS = require( 'aws-sdk' );
-exports.check = ( event, context, callback ) => {
+exports.check = ( event, context, callback ) =>
+{
 	var FunctionName = process.env.FunctionName;
 	var envconfigRuleName = process.env.configRuleName;
 	const message = event.Records[ 0 ].Sns;
@@ -11,13 +12,20 @@ exports.check = ( event, context, callback ) => {
 	console.log( jsonContent );
 	console.log( complianceType + " " + configRuleName );
 	var region = process.env.AWS_REGION;
-	var ec2 = new AWS.EC2( { region: region } );
-	var lambda = new AWS.Lambda( { apiVersion: '2015-03-31' } );
+	var ec2 = new AWS.EC2(
+	{
+		region: region
+	} );
+	var lambda = new AWS.Lambda(
+	{
+		apiVersion: '2015-03-31'
+	} );
 	var messageParams = {
 		FunctionName: FunctionName,
 		Payload: '{"message": "ComplianceType: ' + resourceId + ' ' + complianceType + ' ' + configRuleName + '", "subject": "Config Rules Event ' + configRuleName + '"}'
 	};
-	if ( complianceType == 'NON_COMPLIANT' && configRuleName == envconfigRuleName ) {
+	if ( complianceType == 'NON_COMPLIANT' && configRuleName == envconfigRuleName )
+	{
 		console.log( 'Non Compliant' );
 		var params = {
 			CidrIp: '0.0.0.0/0',
@@ -26,10 +34,16 @@ exports.check = ( event, context, callback ) => {
 			IpProtocol: "TCP",
 			ToPort: 22
 		};
-		ec2.revokeSecurityGroupIngress( params, function( err, data ) {
-			if ( err ) { console.log( err, err.stack ); } // an error occurred
-			else {
-				lambda.invoke( messageParams, function( err, data ) {
+		ec2.revokeSecurityGroupIngress( params, function( err, data )
+		{
+			if ( err )
+			{
+				console.log( err, err.stack );
+			} // an error occurred
+			else
+			{
+				lambda.invoke( messageParams, function( err, data )
+				{
 					if ( err ) console.log( err, err.stack ); // an error occurred
 					else console.log( data ); // successful response
 				} );
